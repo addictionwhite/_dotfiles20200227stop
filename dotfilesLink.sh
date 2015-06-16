9ffaf8468197c974f1b906d0276ddd183b8a7cbc
tsukasa <enigma961@gmail.com>
2015-06-16 23:02:40 +0900

    正しくシンボリックリンクが貼れていない箇所修正 NeoBundleの記述修正

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-ln -sf ~/dotfiles/.vim ~/.vim
+if [[ ! -e ~/.vim ]]; then
+  ln -sf ~/dotfiles/.vim ~/.vim
+fi
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/colors ~/.vim
 ln -sf ~/dotfiles/.gvimrc ~/.gvimrc
